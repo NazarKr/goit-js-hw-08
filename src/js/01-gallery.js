d5400d1db731b99b54b8f13405dee0b872c70cc0
NazarKr <nazarok1990@gmail.com>
2022-11-17 22:32:00 +0200

    update 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const listGalery = document.querySelector('.gallery');
+
+function createGaleryTemplate(items) {
+  return items
+    .map(({ preview, description, original }) => {
+      return `<a class="gallery__item" href="${original}">
+                <img class="gallery__image lazyload blur-up" loading="lazy" data-src="${preview}" src="${preview}" alt="${description}" />
+            </a>`;
+    })
+    .join('');
+}
+
+const galleryMarkup = createGaleryTemplate(galleryItems);
+listGalery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoomFactor: 0.05,
+  enableKeyboard: true,
+  disableScroll: true,
+  fadeSpeed: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const throttle = require('lodash.throttle');
+
+player.on('timeupdate', throttle(setWatchingTime, 1000));
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+function setWatchingTime(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
+
+const getContentOfLocalStorage =
+  localStorage.getItem('videoplayer-current-time') || 0;
+
+player.setCurrentTime(getContentOfLocalStorage);
