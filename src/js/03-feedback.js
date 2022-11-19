612d33069be9f2bc6c7326e79f1fdfaa520f5543
NazarKr <nazarok1990@gmail.com>
2022-11-19 17:44:20 +0200

    Update dz

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,13 @@
 import throttle from 'lodash.throttle';
 
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
-
 const form = document.querySelector('form.feedback-form');
 
 const getValueFromStorage = () =>
   JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}');
+
 const handleInput = event => {
   const { name, value } = event.target;
-  console.log(event);
   const currentFormValue = getValueFromStorage();
 
   localStorage.setItem(
@@ -23,7 +22,6 @@ const handleInput = event => {
 (() => {
   const formValue = localStorage.getItem(LOCAL_STORAGE_KEY);
   if (formValue) {
-    //  set values in form
     const values = JSON.parse(formValue);
     form['email'].value = values.email || '';
     form['message'].value = values.message || '';
@@ -34,9 +32,6 @@ form.addEventListener('input', throttle(handleInput, 500));
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  const currentFormValue = getValueFromStorage();
-  console.log('submit', currentFormValue);
-
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify({}));
   form.reset();
 });
